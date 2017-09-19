c54043b3e4e0118670b305889ae891b819daedce
Simon Legner <Simon.Legner@gmail.com>
2017-09-19 13:36:50 +0200

    fix(routing): routing on gh-pages

--- source/js/views/Client/index.jsx
+++ source/js/views/Client/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from 'views/App';
 import ScrollToTop from 'components/Global/ScrollToTop';
 
+const Router = __reactRouter__ === 'HashRouter' ? HashRouter : BrowserRouter; // eslint-disable-line
+
 export default () => (
-  <BrowserRouter>
+  <Router>
     <ScrollToTop>
       <App />
     </ScrollToTop>
-  </BrowserRouter>
+  </Router>
 );
